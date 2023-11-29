8c1955b598568484dd7628bc7891b8af481b5e8f
HodayaGitHub <hodaya1812@gmail.com>
2023-11-30 00:33:25 +0200

    Add features to the editor and icons

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -95,18 +95,13 @@ function onAddText(ev) {
   gCtx.lineWidth = 2
   gCtx.textAlign = "center"
 
-  const lineHeight = 50
+  const lineHeight = 80
   wrappedText(lineHeight)
+
   // gCtx.fillText(textContent, gCanvas.width / 2, gCanvas.height / 2, gCanvas.width)
   // gCtx.strokeText(gTextContent, gCanvas.width / 2, gCanvas.height / 2, gCanvas.width)
 
 }
-
-
-function onLongText() {
-
-}
-
 
 function renderCanvas() {
   gCtx.fillRect(0, 0, gCanvas.width, gCanvas.height)
@@ -114,86 +109,3 @@ function renderCanvas() {
     renderImageOnCanvas(gSelecedImg)
   }
 }
-
-// function wrappedText() {
-//   let textToIterate = wrapText(gCtx, gTextContent, 85, 200, 1050, 140)
-//   textToIterate.forEach(function (item) {
-//     // item[0] is the text
-//     // item[1] is the x coordinate 
-//     // item[2] is the y coordinate 
-//     gCtx.fillText(item[0], item[1], item[2])
-//     gCtx.strokeText(item[0], item[1], item[2])
-
-//   })
-// }
-
-
-
-
-function wrappedText(lineHeight) {
-  let textToIterate = wrapText(gCtx, gTextContent, gCanvas.width / 2, gCanvas.height / 2, gCanvas.width, lineHeight)
-  textToIterate.forEach(function (item) {
-    gCtx.fillText(item[0], item[1], item[2])
-    gCtx.strokeText(item[0], item[1], item[2])
-  })
-}
-
-
-
-
-function wrapText(ctx, text, x, y, maxWidth, lineHeight) {
-  let words = text.split(' ')
-  let line = '';
-  let testLine = ''
-  let lineArray = []
-
-  for (let n = 0; n < words.length; n++) {
-    testLine += words[n] + ' '
-    let metrics = ctx.measureText(testLine)
-    let testWidth = metrics.width
-
-    if (testWidth > maxWidth && n > 0) {
-      lineArray.push([line.trim(), x, y])
-      y += lineHeight;
-      line = words[n] + ' '
-      testLine = words[n] + ' '
-    } else {
-      line += words[n] + ' '
-    }
-
-    if (n === words.length - 1) {
-      lineArray.push([line.trim(), x, y])
-    }
-  }
-
-  console.log(lineArray)
-  return lineArray
-}
-
-
-
-// function wrapText(ctx, text, x, y, maxWidth, lineHeight) {
-//   let words = text.split(' ')
-//   let line = ''
-//   let testLine = ''
-//   let lineArray = []
-
-//   for (var n = 0; n < words.length; n++) {
-//     testLine += `${words[n]} `
-//     let metrics = ctx.measureText(testLine)
-//     let testWidth = metrics.width
-//     if (testWidth > maxWidth && n > 0) {
-//       lineArray.push([line, x, y]);
-//       y += lineHeight
-//       line = `${words[n]} `
-//       testLine = `${words[n]} `
-//     }
-//     else {
-//       line += `${words[n]} `
-//     }
-//     if (n === words.length - 1) {
-//       lineArray.push([line, x, y])
-//     }
-//   }
-//   return lineArray
-// }
