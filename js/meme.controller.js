e02457fefe6b358da414a1ee9c4e3a5b59f116b1
HodayaGitHub <hodaya1812@gmail.com>
2023-11-29 17:30:01 +0200

    Render images and add grid

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -6,4 +6,25 @@ $(onInit)
 
 function onInit() {
     console.log('test for jquery')
+    renderImages()
   }
+
+
+
+  
+function renderImages() {
+  _createImages()
+  const imageContainer = $('.images-container')
+  // imageContainer.html('')
+
+  gImages.forEach((image, index) => {
+      const imgElement = `
+      <div class="grid-item">
+      <img src="${image.imgUrl}"
+      alt="${image.imgAlt}" 
+      data-index="${index}">
+      </img>
+      </div>`
+      imageContainer.append(imgElement)
+  })
+}
--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -1,26 +1,27 @@
 'use strict'
 let gImages = []
 
-function _createImg(imgUrl, imgAlt){
+function _createImg(imgUrl, imgAlt) {
     return {
-        imgUrl, 
-        imgAlt, 
+        imgUrl,
+        imgAlt,
         imgDesc: null,
     }
 }
 
 
 
-function _createImages(){
-    const numbersArray = Array.from({ length: 19 }, (_, index) => index)
+function _createImages() {
+    const numbersArray = Array.from({ length: 18 }, (_, index) => index)
 
-     gImages = numbersArray.map((index) => {
-        const imgUrl = `img/${index}.png`
-        const imgAlt = `Image ${index}`
+    gImages = numbersArray.map((index) => {
+        const imgUrl = `img/${index + 1}.jpg`
+        const imgAlt = `Image ${index + 1}`
         return _createImg(imgUrl, imgAlt)
     })
-
 }
 
-_createImages()
+
+
+
 console.log(gImages)
