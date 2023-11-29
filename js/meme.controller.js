d5387a027b7f53647750eb86a195421591cae79d
HodayaGitHub <hodaya1812@gmail.com>
2023-11-29 20:29:01 +0200

    Add canvas and render images on canvas

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -1,30 +1,63 @@
 'use strict'
+
+let gCanvas
+let gCtx
+
+
 // onload
 $(onInit)
 
 
 
 function onInit() {
-    console.log('test for jquery')
-    renderImages()
-  }
+  console.log('test for jquery')
+  renderImages()
+  addEventListeners()
+  $('.canvas-container').hide()
+
+  gCanvas = $('canvas')[0]
+  gCtx = gCanvas.getContext('2d')
+}
 
 
 
-  
+
 function renderImages() {
   _createImages()
   const imageContainer = $('.images-container')
   // imageContainer.html('')
 
   gImages.forEach((image, index) => {
-      const imgElement = `
+    const imgElement = `
       <div class="grid-item">
-      <img src="${image.imgUrl}"
+      <img  src="${image.imgUrl}"
       alt="${image.imgAlt}" 
       data-index="${index}">
       </img>
       </div>`
-      imageContainer.append(imgElement)
+    imageContainer.append(imgElement)
   })
+  // id="img#${image.id}"
+}
+
+
+function addEventListeners() {
+  $('.grid-item img').click(function () {
+    renderImageOnCanvas(this)
+  })
+}
+
+
+function renderImageOnCanvas(clickedImg) {
+  $('.images-container').hide()
+  $('.canvas-container').show()
+  // const elImgSrc = clickedImg.currentSrc
+  onSelectImg(clickedImg)
+}
+
+
+
+function onSelectImg(image) {
+    gCtx.drawImage(image, 0, 0, gCanvas.width, gCanvas.height)
+
 }
--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -1,8 +1,9 @@
 'use strict'
 let gImages = []
 
-function _createImg(imgUrl, imgAlt) {
+function _createImg(id, imgUrl, imgAlt) {
     return {
+        id,
         imgUrl,
         imgAlt,
         imgDesc: null,
@@ -15,13 +16,21 @@ function _createImages() {
     const numbersArray = Array.from({ length: 18 }, (_, index) => index)
 
     gImages = numbersArray.map((index) => {
+        var id = `${index + 1}`
         const imgUrl = `img/${index + 1}.jpg`
         const imgAlt = `Image ${index + 1}`
-        return _createImg(imgUrl, imgAlt)
+        return _createImg(id, imgUrl, imgAlt)
     })
+
+    console.log(gImages)
+
 }
 
 
 
 
-console.log(gImages)
+function _saveMemsToStorage() {
+    saveToStorage(STORAGE_KEY, userMems)
+}
+
+
